736649bc271e8b0e67f613bc0fc15d06ee2abdc3
SanThorl <athawmgaj@gmail.com>
2024-03-31 11:34:52 +0630

    Pagination in mvc

--- DotNetCore.mvcApp/Controllers/BlogPaginationController.cs
+++ DotNetCore.mvcApp/Controllers/BlogPaginationController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCore.mvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCore.mvcApp.Controllers
 {
     public class BlogPaginationController : Controller
     {
-        public IActionResult BlogIndex()
+        [ActionName("Index")]
+        public IActionResult BlogIndex(int pageNo=1, int pageSize = 10)
         {
-            AppDbContext db = new AppDbContext(); 
-            return View("BlogIndex");
+            AppDbContext _db = new AppDbContext();
+
+            int rowCount = _db.Blogs.Count();
+
+            int pageCount = rowCount / pageSize;
+            if (rowCount % pageSize > 0)
+                pageCount++;
+            
+            if(pageNo > rowCount)
+            {
+                return Redirect("/Blog");
+            }
+
+            List<BlogModel> lst = _db.Blogs
+                //.OrderByDescending(X => X.BlogId)
+                .Skip((pageNo -1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            BlogResponseModel model = new();
+            model.Data = lst;
+            model.PageSize = pageSize;
+            model.PageNo = pageNo;
+            model.PageCount = pageCount;
+
+            return View("BlogIndex", model);
         }
     }
 }
