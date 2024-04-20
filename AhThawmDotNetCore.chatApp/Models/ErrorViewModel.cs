8f99f8d5b1ed13ce50cb6dfc356b6f2632613b0a
SanThorl <athawmgaj@gmail.com>
2024-04-20 13:48:48 +0630

    Realtime Chat App using SignalR

---
+++ AhThawmDotNetCore.chatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AhThawmDotNetCore.chatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
