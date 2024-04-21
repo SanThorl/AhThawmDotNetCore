9cc0fbdeb6318b83e8ab09594012690c498b4c68
SanThorl <athawmgaj@gmail.com>
2024-04-21 21:31:00 +0630

    Real-time Chart app using signalR

---
+++ AhThawmDotNetCore.realTimeChartApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using AhThawmDotNetCore.realTimeChartApp.Hubs;
+using AhThawmDotNetCore.realTimeChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace AhThawmDotNetCore.realTimeChartApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public HomeController(ILogger<HomeController> logger, IHubContext<NotificationHub> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(DataRequestModel requestModel)
+        {
+             await _hubContext.Clients.All.SendAsync("ClientReceiveEvent", requestModel.Data);
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public class DataRequestModel
+    {
+        public string Data { get; set; }
+    }
+}
---
+++ AhThawmDotNetCore.realTimeChartApp/Hubs/NotificationHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AhThawmDotNetCore.realTimeChartApp.Hubs
+{
+    public class NotificationHub : Hub
+    {
+
+    }
+}
